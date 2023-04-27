ca2a52a60cdebf483dacd5969389db175cdcb5c9
Bangseungjae <tmdwo5825@naver.com>
2023-04-28 00:53:28 +0900

    first init

---
+++ src/main/java/cbnu/io/cbnuswalrami/CbnuSwAlramiApplication.java
@@ -0,0 +1,13 @@
+package cbnu.io.cbnuswalrami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbnuSwAlramiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbnuSwAlramiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cbnu/io/cbnuswalrami/CbnuSwAlramiApplicationTests.java
@@ -0,0 +1,13 @@
+package cbnu.io.cbnuswalrami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbnuSwAlramiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
