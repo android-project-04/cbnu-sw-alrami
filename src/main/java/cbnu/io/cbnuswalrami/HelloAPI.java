5a6ee59e5e700467e2dfb45d17948838076e81d9
Bangseungjae <tmdwo5825@naver.com>
2023-04-30 17:41:12 +0900

    feature: Helloworld API를 개발한다. - 헬스체크용 API를 개발한다. - Spring Security로 해당 API는 허용해준다.

---
+++ src/main/java/cbnu/io/cbnuswalrami/HelloAPI.java
@@ -0,0 +1,14 @@
+package cbnu.io.cbnuswalrami;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloAPI {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
