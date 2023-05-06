c41f91e3467823a176cfbbb03ce37b57171dd63a
Bangseungjae <tmdwo5825@naver.com>
2023-05-07 02:12:24 +0900

    feature: 스프링 시큐리티 관련 서비스 작성

---
+++ src/main/java/cbnu/io/cbnuswalrami/common/configuration/security/service/CustomUserDetailService.java
@@ -0,0 +1,32 @@
+package cbnu.io.cbnuswalrami.common.configuration.security.service;
+
+import cbnu.io.cbnuswalrami.business.core.domon.user.entity.Member;
+import cbnu.io.cbnuswalrami.business.core.domon.user.entity.values.LoginId;
+import cbnu.io.cbnuswalrami.business.core.domon.user.infrastructure.command.MemberJpaRepository;
+import cbnu.io.cbnuswalrami.business.core.error.CommonTypeException;
+import cbnu.io.cbnuswalrami.common.configuration.security.AccountAdapter;
+import cbnu.io.cbnuswalrami.common.exception.common.CbnuException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    public CustomUserDetailService(MemberJpaRepository memberJpaRepository) {
+        this.memberJpaRepository = memberJpaRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("username: {}", username);
+        Member member = memberJpaRepository.findByLoginId(LoginId.from(username))
+                .orElseThrow(() -> CbnuException.of(CommonTypeException.NOT_FOUNT_USER));
+        return new AccountAdapter(member);
+    }
+}
