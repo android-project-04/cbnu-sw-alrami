77a2fadb1774f262aa15a2a4c76c3c635dfe9cb7
Bangseungjae <tmdwo5825@naver.com>
2023-05-07 02:17:11 +0900

    build: helth check api 패키지 경로 수정

---
+++ src/main/java/cbnu/io/cbnuswalrami/business/web/helthcheck/HelloAPI.java
@@ -0,0 +1,16 @@
+package cbnu.io.cbnuswalrami.business.web.helthcheck;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloAPI {
+
+    @GetMapping("/")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("hello5");
+    }
+}
