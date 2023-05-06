bca2de96d86c7691adae6822b3a689bb6492bd9f
Bangseungjae <tmdwo5825@naver.com>
2023-05-07 02:15:05 +0900

    feature: 로그인에 사용하는 요청 응답 클래스 생성

---
+++ src/main/java/cbnu/io/cbnuswalrami/business/web/user/presentation/request/LoginRequest.java
@@ -0,0 +1,25 @@
+package cbnu.io.cbnuswalrami.business.web.user.presentation.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotEmpty(message = "로그인 아이디를 입력하여주세요.")
+    @NotNull(message = "로그인 아이디를 입력하여주세요.")
+    private String loginId;
+
+    @NotEmpty(message = "비밀번호를 입력해주세요.")
+    @NotNull(message = "비밀번호를 입력해주세요.")
+    private String password;
+
+    public LoginRequest(String loginId, String password) {
+        this.loginId = loginId;
+        this.password = password;
+    }
+}
