dbed2b0f0864a482adc88d7ed948509bedaeaa6d
Bangseungjae <tmdwo5825@naver.com>
2023-05-07 02:16:07 +0900

    featuer: 비즈니스 레이어 기능 구현체 개발 - 회원가입 승인 - 로그인 - 회원가입

--- src/main/java/cbnu/io/cbnuswalrami/business/web/user/application/service/SignupService.java
+++ src/main/java/cbnu/io/cbnuswalrami/business/web/user/application/service/SignupService.java
@@ -1,16 +1,17 @@
 package cbnu.io.cbnuswalrami.business.web.user.application.service;
 
-import cbnu.io.cbnuswalrami.business.core.domon.user.entity.Users;
+import cbnu.io.cbnuswalrami.business.core.domon.user.entity.Member;
 import cbnu.io.cbnuswalrami.business.core.domon.user.entity.values.LoginId;
 import cbnu.io.cbnuswalrami.business.core.domon.user.entity.values.Password;
 import cbnu.io.cbnuswalrami.business.core.domon.user.entity.values.StudentNumber;
-import cbnu.io.cbnuswalrami.business.core.domon.user.infrastructure.UserJpaRepository;
+import cbnu.io.cbnuswalrami.business.core.domon.user.infrastructure.command.MemberJpaRepository;
 import cbnu.io.cbnuswalrami.business.web.s3.application.S3Command;
 import cbnu.io.cbnuswalrami.business.web.user.application.SignupCommand;
 import cbnu.io.cbnuswalrami.business.web.user.presentation.request.SignupRequest;
 import cbnu.io.cbnuswalrami.common.exception.common.CbnuException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
@@ -20,34 +21,37 @@ import static cbnu.io.cbnuswalrami.business.core.error.CommonTypeException.*;
 @Service
 public class SignupService implements SignupCommand {
 
-    private final UserJpaRepository userJpaRepository;
+    private final MemberJpaRepository memberJpaRepository;
     private final PasswordEncoder passwordEncoder;
 
     private final S3Command s3Command;
 
-    public SignupService(UserJpaRepository userJpaRepository, PasswordEncoder passwordEncoder, S3Command s3Command) {
-        this.userJpaRepository = userJpaRepository;
+    public SignupService(MemberJpaRepository memberJpaRepository, PasswordEncoder passwordEncoder, S3Command s3Command) {
+        this.memberJpaRepository = memberJpaRepository;
         this.passwordEncoder = passwordEncoder;
         this.s3Command = s3Command;
     }
 
-    public Users signup(SignupRequest signupRequest, MultipartFile file) {
-        System.out.println("============1");
-
+    @Transactional
+    public Member signup(SignupRequest signupRequest, MultipartFile file) {
         signupValidate(signupRequest);
 
         String url = s3Command.uploadFile(file);
-        System.out.println("============2");
+        String encodedPassword = getEncodedPassword(signupRequest.getPassword());
 
 
-        Users users = new Users(
+        Member member = new Member(
                 LoginId.from(signupRequest.getLoginId()),
                 Password.from(signupRequest.getPassword()),
                 StudentNumber.from(signupRequest.getStudentNumber()),
                 url
         );
 
-        return userJpaRepository.save(users);
+        return memberJpaRepository.save(member);
+    }
+
+    private String getEncodedPassword(String password) {
+        return passwordEncoder.encode(password);
     }
 
     private void signupValidate(SignupRequest signupRequest) {
@@ -55,12 +59,12 @@ public class SignupService implements SignupCommand {
             throw CbnuException.of(NULL_EXCEPTION);
         }
 
-        Optional<Users> isExist = userJpaRepository.findByLoginId(LoginId.from(signupRequest.getLoginId()));
+        Optional<Member> isExist = memberJpaRepository.findByLoginId(LoginId.from(signupRequest.getLoginId()));
         if (isExist.isPresent()) {
             throw CbnuException.of(EXIST_USER);
         }
 
-        Optional<Users> byStudentNumber = userJpaRepository.findByStudentNumber(StudentNumber.from(signupRequest.getStudentNumber()));
+        Optional<Member> byStudentNumber = memberJpaRepository.findByStudentNumber(StudentNumber.from(signupRequest.getStudentNumber()));
         if (byStudentNumber.isPresent()) {
             throw CbnuException.of(EXIST_USER);
         }
