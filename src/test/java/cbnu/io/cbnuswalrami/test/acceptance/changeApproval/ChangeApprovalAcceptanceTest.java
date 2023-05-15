6bc8b6657bdc5a814417f61b441fa24d657b518b
Bangseungjae <tmdwo5825@naver.com>
2023-05-16 00:31:16 +0900

    test: 유저 승인 기능 테스트 코드 작성 - 인수 테스트 작성 - fixture 작성

--- src/test/java/cbnu/io/cbnuswalrami/test/acceptance/changeApproval/ChangeApprovalAcceptanceTest.java
+++ src/test/java/cbnu/io/cbnuswalrami/test/acceptance/changeApproval/ChangeApprovalAcceptanceTest.java
@@ -1,23 +1,32 @@
 package cbnu.io.cbnuswalrami.test.acceptance.changeApproval;
 
 import cbnu.io.cbnuswalrami.business.core.domon.user.entity.Member;
-import cbnu.io.cbnuswalrami.business.web.user.application.ApprovalChangeCommand;
-import cbnu.io.cbnuswalrami.business.web.user.application.SignupCommand;
 import cbnu.io.cbnuswalrami.common.configuration.container.DatabaseTestBase;
+import cbnu.io.cbnuswalrami.common.configuration.security.SecurityTestConfig;
 import cbnu.io.cbnuswalrami.test.helper.fixture.MemberFixture;
+import cbnu.io.cbnuswalrami.test.helper.fixture.SignupFixture;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 import org.springframework.restdocs.restassured3.RestAssuredRestDocumentation;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 @DisplayName("유저 승인기능 인수 테스트")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -29,10 +38,7 @@ public class ChangeApprovalAcceptanceTest extends DatabaseTestBase {
     private int port = 8080;
 
     @Autowired
-    private SignupCommand signupCommand;
-
-    @Autowired
-    private ApprovalChangeCommand approvalChangeCommand;
+    private SignupFixture signupFixture;
 
 
     private RequestSpecification documentationSpec = new RequestSpecBuilder().setPort(port).setBaseUri(BASE_URL).build();
@@ -52,18 +58,39 @@ public class ChangeApprovalAcceptanceTest extends DatabaseTestBase {
     }
 
 
-
     @DisplayName("회원가입한 유저가 있을 떄 어드민 계정은 해당 유저를 승인한다.")
     @Test
     public void given_not_approval_member_when_change_approval_then_ok_member() {
         // given
         Member member = MemberFixture.createMember();
+        Member signupMember = signupFixture.signupNonApprovalMember(
+                member.getLoginId().getLoginId(),
+                "SjSj1234!@",
+                member.getNickname().getNickname()
+        );
 
-        // 어드민 로그인
+        String uri = "/api/admin/approval/" + signupMember.getId();
 
         // when - 어드민으로 요청
+        given(documentationSpec)
+                .contentType(ContentType.JSON)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .filter(document(
+                        "change-approval",
+//                        ),
+                        getResponseFieldsSnippet()
+                ))
+                .when()
+                .put(uri)
+                .then().statusCode(HttpStatus.OK.value());
+    }
 
-        // then - Approval이 OK
-
+    private ResponseFieldsSnippet getResponseFieldsSnippet() {
+        return responseFields(
+                fieldWithPath("eventTime").type(JsonFieldType.STRING).description("이벤트 시간"),
+                fieldWithPath("status").type(JsonFieldType.STRING).description("HTTP 상태값"),
+                fieldWithPath("code").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
+                fieldWithPath("data").type(JsonFieldType.NUMBER).description("승인된 유저 id값")
+        );
     }
 }
