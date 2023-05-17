b2ada5dfec718c9cf9e2c78b954e74529dd4957c
Bangseungjae <tmdwo5825@naver.com>
2023-05-17 16:42:20 +0900

    feature: 비승인 멤버 조회 개발

---
+++ src/main/java/cbnu/io/cbnuswalrami/business/web/user/presentation/NoApprovalMemberListAPI.java
@@ -0,0 +1,29 @@
+package cbnu.io.cbnuswalrami.business.web.user.presentation;
+
+import cbnu.io.cbnuswalrami.business.web.common.Cursor;
+import cbnu.io.cbnuswalrami.business.web.common.CursorResult;
+import cbnu.io.cbnuswalrami.business.web.user.application.service.paging.ApprovalCursorPagingService;
+import cbnu.io.cbnuswalrami.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class NoApprovalMemberListAPI {
+
+    private final ApprovalCursorPagingService approvalCursorPagingService;
+
+    @GetMapping("/no/approval/members")
+    public ResponseEntity<ApiResponse> findNoApprovalList(@RequestParam Long next, @RequestParam Long size) {
+        //TODO
+        CursorResult cursorResult = approvalCursorPagingService.findMemberDtosByCursor(Cursor.from(next, size));
+        return ResponseEntity
+                .ok()
+                .body(ApiResponse.of(cursorResult));
+    }
+}
