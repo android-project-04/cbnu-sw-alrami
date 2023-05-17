32b952f93391e0e1d998b3ce7611b29f692fec72
Bangseungjae <tmdwo5825@naver.com>
2023-05-17 21:21:44 +0900

    feature: 에러 핸들러 기능 개발

---
+++ src/main/java/cbnu/io/cbnuswalrami/common/exception/ExceptionHandler.java
@@ -0,0 +1,30 @@
+package cbnu.io.cbnuswalrami.common.exception;
+
+import cbnu.io.cbnuswalrami.common.exception.common.CbnuException;
+import cbnu.io.cbnuswalrami.common.exception.common.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResponse catchIllegalArgumentException(IllegalArgumentException exception) {
+        log.info("IllegalArgumentException: {}", exception.getMessage());
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(CbnuException.class)
+    public ErrorResponse catchCbnuException(CbnuException exception) {
+        log.info("CbnuException: {}", exception.getMessage());
+        return ErrorResponse.of(exception);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(RuntimeException.class)
+    public ErrorResponse catchRuntimeException(IllegalArgumentException exception) {
+        log.info("RuntimException: {}", exception.getMessage());
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+}
