437965219e80b2f76331a331ea5a3bbb7206692e
Bangseungjae <tmdwo5825@naver.com>
2023-05-23 17:25:46 +0900

    feature: 커뮤니티 페이징 조회 오래된순 기능을 개발한다.

--- src/main/java/cbnu/io/cbnuswalrami/business/core/domon/post/community/infrastructure/query/CommunityQuery.java
+++ src/main/java/cbnu/io/cbnuswalrami/business/core/domon/post/community/infrastructure/query/CommunityQuery.java
@@ -60,4 +60,45 @@ public class CommunityQuery {
                 .fetch();
         return fetch.size() > 0L;
     }
+
+    public Boolean existOldCommunityId(Long id) {
+        List<Long> fetch = queryFactory.select(community.id)
+                .from(community)
+                .where(community.isDeleted.eq(FALSE).and(community.id.gt(id)))
+                .limit(1)
+                .fetch();
+        return fetch.size() > 0L;
+    }
+
+    public List<ResponseCommunity> findResponseOldCommunities(Long next, Long size) {
+        return queryFactory.select(new QResponseCommunity(
+                        community.id,
+                        community.title.title,
+                        community.description.description,
+                        community.url,
+                        community.createdAt,
+                        communityCount.count
+                ))
+                .from(community, communityCount)
+                .where(community.id.gt(next).and(community.isDeleted.eq(FALSE)).and(communityCount.community.id.eq(community.id)))
+                .orderBy(community.id.asc())
+                .limit(size)
+                .fetch();
+    }
+
+    public List<ResponseCommunity> findResponseOldCommunities(Long size) {
+        return queryFactory.select(new QResponseCommunity(
+                        community.id,
+                        community.title.title,
+                        community.description.description,
+                        community.url,
+                        community.createdAt,
+                        communityCount.count
+                ))
+                .from(community, communityCount)
+                .where(community.isDeleted.eq(FALSE).and(communityCount.community.id.eq(community.id)))
+                .orderBy(community.id.asc())
+                .limit(size)
+                .fetch();
+    }
 }
