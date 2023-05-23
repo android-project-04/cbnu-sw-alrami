308667250409e23d21f03cf8e42a93fb4bbef7ce
Bangseungjae <tmdwo5825@naver.com>
2023-05-24 02:08:22 +0900

    feature: Redis에 사용할 값들을 객체로 만든다.

---
+++ src/main/java/cbnu/io/cbnuswalrami/business/web/util/RedisKey.java
@@ -0,0 +1,7 @@
+package cbnu.io.cbnuswalrami.business.web.util;
+
+public class RedisKey {
+    public static final String MEMBER_KEY = "member:";
+    public static final String COMMUNITY_KEY = "community:";
+    public static final String COMMUNITY_COUNT_KEY = "community_count:";
+}
