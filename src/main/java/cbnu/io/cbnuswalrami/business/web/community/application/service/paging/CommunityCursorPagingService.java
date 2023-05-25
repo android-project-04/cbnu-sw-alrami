62a7242fe07a401cfe68e42a621c379cb67af056
Bangseungjae <tmdwo5825@naver.com>
2023-05-25 11:02:14 +0900

    feature: 취업정보 페이징조회 (최신순) 서비스레이어 개발

--- src/main/java/cbnu/io/cbnuswalrami/business/web/community/application/service/paging/CommunityCursorPagingService.java
+++ src/main/java/cbnu/io/cbnuswalrami/business/web/community/application/service/paging/CommunityCursorPagingService.java
@@ -1,6 +1,7 @@
 package cbnu.io.cbnuswalrami.business.web.community.application.service.paging;
 
 import cbnu.io.cbnuswalrami.business.core.domon.post.community.infrastructure.query.CommunityQuery;
+import cbnu.io.cbnuswalrami.business.core.domon.post.values.CommunityType;
 import cbnu.io.cbnuswalrami.business.web.common.Cursor;
 import cbnu.io.cbnuswalrami.business.web.common.CursorResult;
 import cbnu.io.cbnuswalrami.business.web.community.presentation.response.ResponseCommunity;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static cbnu.io.cbnuswalrami.business.core.domon.post.values.CommunityType.*;
 
 @Service
 @RequiredArgsConstructor
@@ -20,12 +23,12 @@ public class CommunityCursorPagingService {
     public CursorResult findByCursor(Cursor cursor) {
         List<ResponseCommunity> responseCommunities = getResponseCommunities(cursor);
         Long lastIndex = responseCommunities.isEmpty() ? 0 : responseCommunities.get(responseCommunities.size() - 1).getId();
-        Boolean hasNext  = hasNext(lastIndex);
+        Boolean hasNext  = hasNext(lastIndex, COMMUNITY);
         return CursorResult.from(responseCommunities, hasNext, lastIndex);
     }
 
-    private Boolean hasNext(Long lastIndex) {
-        return communityQuery.existByCommunityId(lastIndex);
+    private Boolean hasNext(Long lastIndex, CommunityType communityType) {
+        return communityQuery.existByCommunityId(lastIndex, communityType);
     }
 
     private List<ResponseCommunity> getResponseCommunities(Cursor cursor) {
@@ -33,5 +36,20 @@ public class CommunityCursorPagingService {
             return communityQuery.findResponseCommunities(cursor.getSize());
         }
         return communityQuery.findResponseCommunities(cursor.getNext(), cursor.getSize());
+    }
+
+    @Transactional(readOnly = true)
+    public CursorResult findEmploymentCommunity(Cursor cursor) {
+        List<ResponseCommunity> responseCommunities = getResponseCommunityByEmploymentCommunityCursor(cursor);
+        Long lastIndex = responseCommunities.get(responseCommunities.size() - 1).getId();
+        Boolean hasNext = hasNext(lastIndex, EMPLOYMENT);
+        return CursorResult.from(responseCommunities, hasNext, lastIndex);
+    }
+
+    private List<ResponseCommunity> getResponseCommunityByEmploymentCommunityCursor(Cursor cursor) {
+        if (cursor.getNext() == null) {
+            return communityQuery.findResponseEmploymentCommunities(cursor.getSize());
+        }
+        return communityQuery.findResponseEmploymentCommunities(cursor.getNext(), cursor.getSize());
     }
 }
