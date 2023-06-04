3dddbaad642c4bd022a47ad75d8011b7a189912a
Bangseungjae <tmdwo5825@naver.com>
2023-06-04 16:37:17 +0900

    feature: 조회수를 커뮤니티를 가지고 조회하는 쿼리 개발

--- src/main/java/cbnu/io/cbnuswalrami/business/core/domon/post/community_count/infrastructure/CommunityCountJpaRepository.java
+++ src/main/java/cbnu/io/cbnuswalrami/business/core/domon/post/community_count/infrastructure/CommunityCountJpaRepository.java
@@ -1,9 +1,14 @@
 package cbnu.io.cbnuswalrami.business.core.domon.post.community_count.infrastructure;
 
+import cbnu.io.cbnuswalrami.business.core.domon.post.community.entity.Community;
 import cbnu.io.cbnuswalrami.business.core.domon.post.community_count.entity.CommunityCount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CommunityCountJpaRepository extends JpaRepository<CommunityCount, Long> {
+
+    Optional<CommunityCount> findByCommunity(Community community);
 }
