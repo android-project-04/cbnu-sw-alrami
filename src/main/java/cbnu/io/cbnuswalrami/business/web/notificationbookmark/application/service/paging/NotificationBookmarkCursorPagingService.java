bb85515961e86cc7fb252aea1658b5dedfdc0deb
Bangseungjae <tmdwo5825@naver.com>
2023-06-05 04:15:04 +0900

    fix: Member를 파라미터로 받고 해당 member의 즐겨찾기만 가져오게한다.

--- src/main/java/cbnu/io/cbnuswalrami/business/web/notificationbookmark/application/service/paging/NotificationBookmarkCursorPagingService.java
+++ src/main/java/cbnu/io/cbnuswalrami/business/web/notificationbookmark/application/service/paging/NotificationBookmarkCursorPagingService.java
@@ -1,5 +1,6 @@
 package cbnu.io.cbnuswalrami.business.web.notificationbookmark.application.service.paging;
 
+import cbnu.io.cbnuswalrami.business.core.domon.member.entity.Member;
 import cbnu.io.cbnuswalrami.business.core.domon.notificationbookmark.infrastructure.query.NotificationBookmarkQuery;
 import cbnu.io.cbnuswalrami.business.web.common.Cursor;
 import cbnu.io.cbnuswalrami.business.web.common.CursorResult;
@@ -17,9 +18,9 @@ public class NotificationBookmarkCursorPagingService {
     private final NotificationBookmarkQuery notificationBookmarkQuery;
 
     @Transactional(readOnly = true)
-    public CursorResult findNotificationBookmarksByCursor(Cursor cursor) {
+    public CursorResult findNotificationBookmarksByCursor(Cursor cursor, Member member) {
 
-        List<NotificationDto> notificationDtos = getNotificationDtos(cursor.getNext(), cursor.getSize());
+        List<NotificationDto> notificationDtos = getNotificationDtos(cursor.getNext(), cursor.getSize(), member);
         Long lastIndex = notificationDtos.isEmpty() ? 0 : notificationDtos.get(notificationDtos.size() - 1).getId();
         Boolean hasNext = hasNext(lastIndex);
         CursorResult cursorResult = CursorResult.from(notificationDtos, hasNext, lastIndex);
@@ -27,15 +28,19 @@ public class NotificationBookmarkCursorPagingService {
         return cursorResult;
     }
 
-    private List<NotificationDto> getNotificationDtos(Long next, Long size) {
+    public List<NotificationDto> getNotificationDtos(
+            Long next,
+            Long size,
+            Member member
+    ) {
         if (next == null) {
-            return notificationBookmarkQuery.findNotificationBookmarkByCursor(size);
+            return notificationBookmarkQuery.findNotificationBookmarkByCursor(size, member);
         }
-        return notificationBookmarkQuery.findNotificationBookmarkByCursor(next, size);
+        return notificationBookmarkQuery.findNotificationBookmarkByCursor(next, size, member);
     }
 
-    private Boolean hasNext(Long lastIndex) {
-        if (lastIndex >= 1) {
+    public Boolean hasNext(Long lastIndex) {
+        if (lastIndex <= 1) {
             return false;
         }
         return notificationBookmarkQuery.existNotificationBookmarkById(lastIndex);
--- src/main/java/cbnu/io/cbnuswalrami/business/web/notificationbookmark/presentation/NotificationCursorPagingAPI.java
+++ src/main/java/cbnu/io/cbnuswalrami/business/web/notificationbookmark/presentation/NotificationCursorPagingAPI.java
@@ -1,8 +1,10 @@
 package cbnu.io.cbnuswalrami.business.web.notificationbookmark.presentation;
 
+import cbnu.io.cbnuswalrami.business.core.domon.member.entity.Member;
 import cbnu.io.cbnuswalrami.business.web.common.Cursor;
 import cbnu.io.cbnuswalrami.business.web.common.CursorResult;
 import cbnu.io.cbnuswalrami.business.web.notificationbookmark.application.service.paging.NotificationBookmarkCursorPagingService;
+import cbnu.io.cbnuswalrami.business.web.util.MemberFindUtil;
 import cbnu.io.cbnuswalrami.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Nullable;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,12 +23,19 @@ public class NotificationCursorPagingAPI {
 
     private final NotificationBookmarkCursorPagingService notificationBookmarkCursorPagingService;
 
+    private final MemberFindUtil memberFindUtil;
+
     @GetMapping
     public ResponseEntity<ApiResponse> findNotificationBookmarkByCursorPaging(
             @RequestParam @Nullable Long next,
-            @RequestParam @Nullable Long size
+            @RequestParam @Nullable Long size,
+            HttpServletRequest request
             ) {
-        CursorResult cursorResult = notificationBookmarkCursorPagingService.findNotificationBookmarksByCursor(Cursor.from(next, size));
+        Member member = memberFindUtil.findMemberByBearerToken(request);
+        CursorResult cursorResult = notificationBookmarkCursorPagingService.findNotificationBookmarksByCursor(
+                Cursor.from(next, size),
+                member
+        );
 
         return ResponseEntity
                 .ok()
